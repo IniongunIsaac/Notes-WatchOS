df206cae84f077a6b941439d30c59c49da46c5c0
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-29 19:21:19 +0100

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Isaac Iniongun on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
